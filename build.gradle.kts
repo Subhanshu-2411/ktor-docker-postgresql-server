60db8d67bd4660cd5f8bc108d41220eed7733391
Subhanshu Bansal <subhashu20135@iiitd.ac.in>
2023-04-11 18:53:17 +0530

    Added Ktorm and PostgreSQL to Ktor

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,8 @@
 val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
+val ktorm_version: String by project
+val postgresql_driver_version: String by project
 
 plugins {
     kotlin("jvm") version "1.8.20"
@@ -29,4 +31,8 @@ dependencies {
     implementation("ch.qos.logback:logback-classic:$logback_version")
     testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+
+    implementation("org.ktorm:ktorm-core:$ktorm_version")
+    implementation("org.ktorm:ktorm-support-postgresql:$ktorm_version")
+    implementation("org.postgresql:postgresql:$postgresql_driver_version")
 }
